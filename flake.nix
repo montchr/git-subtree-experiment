b3010235a56b3afc34eec0fe0e26b06f9f4b8b48
Chris Montgomery <chris@alley.co>
2021-09-30 19:47:54 -0400

    add flake template files

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "TODO";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    (utils.lib.eachDefaultSystem (system:
+      let
+        inherit (lib) attrValues;
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+        package = with pkgs; callPackage ./. { inherit pkgs; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ ];
+        };
+        
+        defaultPackage = package;
+        
+    })) // {
+      defaultTemplate = self.templates.thisTemplate;
+      templates.thisTemplate.path = ./.;
+      templates.thisTemplate.description = "A default flake template";
+    };
+}
